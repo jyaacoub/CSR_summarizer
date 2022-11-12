612402ee8b5a203225710e7479998e490000fdf2
randyd03 <dingrandy6@gmail.com>
2022-11-12 16:54:32 -0500

    add comments

--- file_reader/pdf_reader.py
+++ file_reader/pdf_reader.py
@@ -1,15 +1,16 @@
+#uses metadata to find page numbers of each section and stores the section header as the key and content as value
+
 from PyPDF2 import PdfReader, PdfFileReader, PdfFileWriter
 #from common.constants import GOOGLE_CSR
 
 GOOGLE_CSR = "file_reader/google-2022-environmental-report.pdf" #import Google CSR file, update the path to constants.py later
 pdf = PdfFileReader(GOOGLE_CSR)
 
-#uses metadata to find page numbers of each section and stores the section header as the key and content as value
 toc_dict = {} #where dictionary is stored
 page_num = [] #stores the page numbers of each section
 
 print("extracting page numbers for each section...")
-for outline in pdf.outlines:
+for outline in pdf.outlines: # adds all the section page numbers into a 2d array [page number, section title]
     try:
         page_num.append([pdf.getDestinationPageNumber(outline)+1, outline.title])
     except:
@@ -19,7 +20,7 @@ page_num.append([pdf.numPages, "Last Page"]) # add the last page of the document
 print(page_num)
 print('')
 
-for i in range(len(page_num)-1):
+for i in range(len(page_num)-1): #loop for adding all the content into the dictionary
     current_section_start = page_num[i][0]
     next_section_start = page_num[i+1][0]
     section_title = page_num[i][1]
@@ -43,6 +44,7 @@ for i in range(len(page_num)-1):
 
 
 
+#UNUSED CODE FOR EXTRACTING PDF INTO FILE----------------------------------------------------------------------------------
 # with open('file_reader/CSR_text.txt', 'w', encoding="utf-8") as file:
 #     for page_num in range(pdf.numPages):
 #         pageObj = pdf.getPage(page_num)
