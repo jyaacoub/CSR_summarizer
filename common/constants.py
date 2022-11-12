0c27ddeed4c2c57a86723eb128fadba18cdcc9e0
jyaacoub <jyaacoub21@gmail.com>
2022-11-12 12:45:20 -0500

    initial ideas for text summarization

--- common/constants.py
+++ common/constants.py
@@ -1,2 +1,2 @@
 CSR_PATH = "data/CSR_samples/"
-GOOGLE_CSR = CSR_PATH + "google-2022-environmental-report.pdf"
+GOOGLE_CSR = CSR_PATH + "google-2022-environmental-report.pdf"  # from https://www.gstatic.com/gumdrop/sustainability/google-2022-environmental-report.pdf
