5f693f18266cbc3bba7f4900bdd8d0009e121856
randyd03 <dingrandy6@gmail.com>
2022-11-13 13:19:03 -0500

    add chunk paragraph method, not complete

--- file_parser/chunker.py
+++ file_parser/chunker.py
@@ -2,6 +2,11 @@
 Used for smartly splitting up sections that are sent by the *file_reader*.
   - Here is where we might want to create further subsections from the sections given by `PDFReader` 
 """
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Embedding, LSTM, Concatenate, Dense
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+
 from common.constants import CHAR_PER_TOKEN
 
 """
@@ -23,7 +28,7 @@ class Chunker:
     def __init__(self, max_tokens=2024):
         self.max_tokens = max_tokens # max number of tokens in a chunk
 
-    #Cuts off a chunk at every '.' and stores it in chunked_list as well as returns the list
+    #Cuts off a chunk at every '.' and return the chunked list
     def chunk_sentence(self, text:str):
         # first check to see if we even need to chunk (is the text longer than max_tokens?)
         if len(text) / CHAR_PER_TOKEN <= self.max_tokens:
@@ -35,3 +40,11 @@ class Chunker:
 
         pass
     
+    #Cuts off a chunk at every ".\n", which detects the end of a paragraph and returns the chunked list
+    #If a paragraph is too large to not be parsed, 
+    def chunk_paragraph(self, text:str):
+         # first check to see if we even need to chunk (is the text longer than max_tokens?)
+        if len(text) / CHAR_PER_TOKEN <= self.max_tokens:
+            return [text]
+
+        pass
