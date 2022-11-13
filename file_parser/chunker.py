b45a87f66b5bd796412ae0443efcc59a0c637e32
jyaacoub <jyaacoub21@gmail.com>
2022-11-13 13:13:51 -0500

    fixed chunk_sentence

--- file_parser/chunker.py
+++ file_parser/chunker.py
@@ -3,35 +3,56 @@ Used for smartly splitting up sections that are sent by the *file_reader*.
   - Here is where we might want to create further subsections from the sections given by `PDFReader` 
 """
 from common.constants import CHAR_PER_TOKEN
+from models.token_counter import TokenCounter
 
 """
-        Chunks text into smaller sections that are less than max_tokens in length
-        
-        Cutoff needs to be chosen to preserve semantic meaning of the text
-            - Cutoff will be at the end of a sentence?
-            - end of a paragraph?
-        
-        I found this resource that might be helpful on how to determine this: https://towardsdatascience.com/nlp-splitting-text-into-sentences-7bbce222ef17
-        
-        Args:
-            text (str): Long string that needs to be chunked
-            
-        Returns:
-            list: list of strings that are the chunks of text
-        """
-class Chunker:
-    def __init__(self, max_tokens=2024):
-        self.max_tokens = max_tokens # max number of tokens in a chunk
+Chunks text into smaller sections that are less than max_tokens in length
+Cutoff needs to be chosen to preserve semantic meaning of the text
+    - Cutoff will be at the end of a sentence?
+    - end of a paragraph?
+I found this resource that might be helpful on how to determine this: https://towardsdatascience.com/nlp-splitting-text-into-sentences-7bbce222ef17
+Args:
+    text (str): Long string that needs to be chunked
 
-    #Cuts off a chunk at every '.' and stores it in chunked_list as well as returns the list
-    def chunk_sentence(self, text:str):
+Returns:
+    list: list of strings that are the chunks of text
+"""
+
+class Chunker:
+    def __init__(self, max_tokens=2024, token_counter:TokenCounter=None):
+        self.max_tokens = max_tokens # max number of tokens in a chunk
+        self.token_counter = token_counter if token_counter is not None else TokenCounter()
+
+    
+    def chunk_sentence(self, text:str, exact=False):
+        """
+        cuttoff at last sentence before max_tokens
+
+        Args:
+            text (str): text to chunk
+            exact (bool, optional): using gpt2 to get exact token counts. Defaults to False.
+
+        Returns:
+            list: list of chunks
+        """
+        
         # first check to see if we even need to chunk (is the text longer than max_tokens?)
-        if len(text) / CHAR_PER_TOKEN <= self.max_tokens:
+        tkn_count = self.token_counter(text, exact=exact)
+        
+        if tkn_count < self.max_tokens:
             return [text]
         else: # chunk then pass remaining to be chunked again
-            max_char_i = int(self.max_tokens * CHAR_PER_TOKEN)
-            return [text[:max_char_i]] + self.chunk(text[max_char_i:])
-        
-        
-        pass
-    
+            max_idx = self.max_tokens*CHAR_PER_TOKEN
+            new_tkn_count = self.token_counter(text[:max_idx], exact=True)
+            # NOTE: the true max_idx could change depending on the text
+            # to determine this algorithmically we would need to try smaller and smaller chunks 
+            # until we get a chunk that is less than max_tokens (can be expensive)
+            assert new_tkn_count <= self.max_tokens, \
+                f"Chunker: Max index  is too large ({max_idx}: \
+                    {new_tkn_count} > {self.max_tokens}). Heuristic method failed."
+            
+            # finding the last period in the first chunk < max_tokens
+            period_idx = text.rfind('.', 0, )
+            if period_idx == -1: # if no period found, then just cut off at max_tokens
+                period_idx = max_idx
+            return [text[:period_idx]] + self.chunk(text[period_idx:])
