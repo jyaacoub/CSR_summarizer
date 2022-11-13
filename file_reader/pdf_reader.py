1b9707543f264a5ae9d19b1fdc7d6ec0abcf2d79
randyd03 <dingrandy6@gmail.com>
2022-11-12 19:19:28 -0500

    create reader into a function

--- file_reader/pdf_reader.py
+++ file_reader/pdf_reader.py
@@ -1,45 +1,45 @@
 #uses metadata to find page numbers of each section and stores the section header as the key and content as value
-
 from PyPDF2 import PdfReader, PdfFileReader, PdfFileWriter
-#from common.constants import GOOGLE_CSR
 
-GOOGLE_CSR = "file_reader/google-2022-environmental-report.pdf" #import Google CSR file, update the path to constants.py later
-pdf = PdfFileReader(GOOGLE_CSR)
+def pdf_reader(file_path):
+    pdf = PdfFileReader(file_path)
 
-toc_dict = {} #where dictionary is stored
-page_num = [] #stores the page numbers of each section
+    toc_dict = {} #where dictionary is stored
+    page_num = [] #stores the page numbers of each section
 
-print("extracting page numbers for each section...")
-for outline in pdf.outlines: # adds all the section page numbers into a 2d array [page number, section title]
-    try:
-        page_num.append([pdf.getDestinationPageNumber(outline)+1, outline.title])
-    except:
-        pass
-
-page_num.append([pdf.numPages, "Last Page"]) # add the last page of the document
-print(page_num)
-print('')
-
-for i in range(len(page_num)-1): #loop for adding all the content into the dictionary
-    current_section_start = page_num[i][0]
-    next_section_start = page_num[i+1][0]
-    section_title = page_num[i][1]
-
-    for current_page in range(current_section_start, max(next_section_start, current_section_start+1)):
-        print(f"Current Page iterating: {current_page}")
-        pageObj = pdf.getPage(current_page)
+    print("extracting page numbers for each section...")
+    for outline in pdf.outlines: # adds all the section page numbers into a 2d array [page number, section title]
         try:
-            txt = pageObj.extract_text()
-            if toc_dict.get(section_title) is not None:
-                toc_dict[section_title] = toc_dict[section_title] + txt
-            else:
-                toc_dict[section_title] = txt
+            page_num.append([pdf.getDestinationPageNumber(outline)+1, outline.title])
         except:
             pass
-    print(section_title)
-    print(''.center(100, '-'))
-    print(toc_dict[section_title])
-    print('\n')
+
+    page_num.append([pdf.numPages, "Last Page"]) # add the last page of the document
+    print(page_num)
+    print('')
+
+    for i in range(len(page_num)-1): #loop for adding all the content into the dictionary
+        current_section_start = page_num[i][0]
+        next_section_start = page_num[i+1][0]
+        section_title = page_num[i][1]
+
+        for current_page in range(current_section_start, max(next_section_start, current_section_start+1)):
+            print(f"Current Page iterating: {current_page}")
+            pageObj = pdf.getPage(current_page)
+            try:
+                txt = pageObj.extract_text()
+                if toc_dict.get(section_title) is not None:
+                    toc_dict[section_title] = toc_dict[section_title] + txt
+                else:
+                    toc_dict[section_title] = txt
+            except:
+                pass
+        print(section_title)
+        print(''.center(100, '-'))
+        print(toc_dict[section_title])
+        print('\n')
+    
+    return toc_dict
                 
 
 
